eea43132f27a471846c8d56910775ce2ff173183
Roleeygit <juhaszroli2001@gmail.com>
2022-12-21 23:44:27 +0100

    Feladat 0104 kész

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,11 @@
+/* File: app.component.ts
+Author: Juhász Roland
+Copyright: 2022, Juhász Roland
+Group: Szoft II N
+Date: 2022-12-21
+Github: https://github.com/Roleygit
+Licenc: GNU GPL  */
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +15,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'latogatni';
+  name = 'Juhász Roland'; 
+  class = 'Szoft II N';
+  date = '2022-12-21';
+
+  theme: string = 'white';
+
+  switchTheme() 
+  {
+    this.theme = this.theme === 'white' ? 'black' : 'white';
+    localStorage.setItem('theme', this.theme);
+  }
+
+
+  cities = [
+    {
+      name: 'Székesfehérvár',
+      county: 'Fejér',
+      area: 170.89,
+      population: 95545
+    },
+    {
+      name: 'Szombathely',
+      county: 'Vas',
+      area: 97.5,
+      population: 78324
+    },
+    {
+      name: 'Sopron',
+      county: 'Győr-Moson-Sopron',
+      area: 169.04,
+      population: 62900
+    },
+    {
+      name: 'Zalaegerszeg',
+      county: 'Zala',
+      area: 102.42,
+      population: 55470
+    },
+    {
+      name: 'Sárvár',
+      county: 'Vas',
+      area: 64.65,
+      population: 15072
+    },
+    {
+      name: 'Lenti',
+      county: 'Zala',
+      area: 73.79,
+      population: 7348
+    },
+    {
+      name: 'Zalakaros',
+      county: 'Zala',
+      area: 17.17,
+      population: 1936
+    },
+
+  ];
+
 }
